15be42eb28071957bba1993b61e48aebcbb1f414
Bruce Canedy <canedy@msn.com>
2022-07-16 03:32:29 +0000

    Chapter 4

--- emerald-dapp/pages/index.js
+++ emerald-dapp/pages/index.js
@@ -1,21 +1,62 @@
+import * as fcl from "@onflow/fcl"
+
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Nav from '../components/Nav.jsx';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
 
   const [newGreeting, setNewGreeting] = useState('');
+  const [greeting, setGreeting] = useState('');
 
-  function runTransaction() {
-    // fcl.authenticate();
-    console.log(newGreeting)
+  async function runTransaction() {
+    const transactionId = await fcl.mutate({
+      cadence: `
+      import HelloWorld from 0x7ed59c7d7d01a49d // THIS WAS MY ADDRESS, USE YOURS
+  
+      transaction(myNewGreeting: String) {
+  
+        prepare(signer: AuthAccount) {}
+  
+        execute {
+          HelloWorld.changeGreeting(newGreeting: myNewGreeting)
+        }
+      }
+      `,
+      args: (arg, t) => [
+        arg(newGreeting, t.String)
+      ],
+      proposer: fcl.authz,
+      payer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 999
+    })
+  
+    console.log("Here is the transactionId: " + transactionId);
+    await fcl.tx(transactionId).onceSealed();
+    executeScript();
   }
 
-  function printGoodbye() {
-    console.log("Goodbye.")
+  async function executeScript() {
+    const response = await fcl.query({
+      cadence: `
+      import HelloWorld from 0x7ed59c7d7d01a49d
+  
+      pub fun main(): String {
+          return HelloWorld.greeting
+      }
+      `, 
+      args: (arg, t) => [] // ARGUMENTS GO IN HERE
+    })
+  
+    setGreeting(response);
   }
+
+  useEffect(() => {
+    executeScript()
+  }, [])
 
   return (
     <div>
@@ -27,15 +68,18 @@ export default function Home() {
 
       <Nav />
 
-      <main className={styles.main}>
+      <div className={styles.welcome}>
         <h1 className={styles.title}>
           Welcome to my <a href="https://academy.ecdao.org" target="_blank">Emerald DApp!</a>
         </h1>
-        <p>This is a DApp created by Jacob Tucker.</p>
+        <p>This is a DApp created by Jacob Tucker (<i>tsnakejake#8364</i>).</p>
+      </div>
 
+      <main className={styles.main}>
+        <p>{greeting}</p>
         <div className={styles.flex}>
-          <button onClick={runTransaction}>Run Transaction</button>
           <input onChange={(e) => setNewGreeting(e.target.value)} placeholder="Hello, Idiots!" />
+          <button onClick={runTransaction}>Run Transaction</button>
         </div>
       </main>
     </div>
